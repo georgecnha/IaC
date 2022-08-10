8c963786896b26ae1c5b8835cc34a277daddcfcb
George <george.pontes02@gmail.com>
2022-08-10 00:00:59 -0300

    Foi retiradq a opção -crypt, que não é obrigatória e não está mais presente na versão atual (3.0) do openssl.

--- infra_como_codigo.sh
+++ infra_como_codigo.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-echo "INFRAESTRUTURA COMO CÓDIGO"
-echo 'Um desafio de código do bootcamp Imersão Linux'
-echo "........................................................"
+echo "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=+=+=+=+=+=+=+=+="
+echo "INFRAESTRUTURA COMO CÓDIGO - Um desafio de código do bootcamp Imersão Linux'
+echo "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=+=+=+=+=+=+=+=+="
+
 echo "Criando grupos de usuários..."
+
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
-echo ''
+
 echo "==========================================================================================="
-echo ''
 echo "Criando usuários a adicionando-os aos seus respectivos grupos..."
-echo ''
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd josefino -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd joao -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd debora -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd josefino -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+
 echo "==========================================================================================="
-echo ''
 echo "Criando pastas..."
+
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
-echo ''
 echo "==========================================================================================="
-echo ''
+
 echo "Determinando permissões das pastas..."
 echo "Passo 1: Atribuindo donos e grupos..."
 chown root:GRP_ADM /adm
@@ -41,7 +42,5 @@ chmod 777 /publico
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
-echo ''
+
 echo "================================FIM DO SCRIPT======================================="
-
-
